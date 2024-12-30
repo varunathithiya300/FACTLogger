2bac3cbc525ceef5ec55d1607e3abdb12e5b92c2
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2024-12-30 22:04:12 +0530

    sync up

--- script.js
+++ script.js
@@ -115,11 +115,52 @@ console.log(category);
 console.log(createdIn);
 
 // Looping through arrays
-
 // .forEach() - This method executes a provided function once for each array element. It does not return anything. -> undefined. Chaining is not possible.
 // .map() - This method creates a new array populated with the results of calling a provided function on every element in the calling array. Chaining is possible.
+
 [2, 3, 4, 5].forEach(function (el) {
   console.log(el);
+});
+
+const times10 = [2, 3, 4, 5].map((el) => el * 10);
+console.log(times10);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const ageOfFacts = initialFacts.forEach(function (el) {
+  console.log(calcFactAge(el.createdIn));
 });
 
 const CATEGORIES = [
@@ -135,8 +176,10 @@ const CATEGORIES = [
 
 const categoryNames = CATEGORIES.map((el) => el.name);
 console.log(categoryNames);
+
 const allCategoryNames = CATEGORIES.forEach(function (el) {
   console.log(el.name);
 });
+
 const allColorCodes = CATEGORIES.map((el) => el.color);
 console.log(allColorCodes);
