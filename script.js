6a04b7774edfa076efdcafbaa3e7035ccab113c6
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-01-04 15:11:24 +0530

    inputting facts dynamically

--- script.js
+++ script.js
@@ -1,10 +1,76 @@
-// Displaying and closing the form
+// Using vanilla JavaScript, for working with DOM elements can become very difficult.
+// For this reason React.js will be useful.
+// In javascript, we can call functions even before they are declared. This concept is called hoisting.
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
 
+// Create DOM elements: Render facts in list
 factsList.innerHTML = "";
 
+// factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+      <p>
+        ${fact.text}
+        <a 
+          class="source" 
+          href="${fact.source}" 
+          target="_blank">(Source)
+        </a>
+      </p>
+      <span 
+        class="tags" 
+        style="background-color: #3b82f6">${fact.category}
+      </span>
+    </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+createFactsList(initialFacts);
+// createFactsList([{ text: "Jonas" }]);
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   console.log("CLICK");
   if (form.classList.contains("hidden")) {
@@ -16,7 +82,7 @@ btn.addEventListener("click", function () {
   }
 });
 
-console.dir(btn);
+// console.dir(btn);
 
 /*
 
